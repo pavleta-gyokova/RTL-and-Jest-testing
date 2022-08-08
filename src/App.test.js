7c83a4260dcb964151e01a5b8e621cdaef3090be
Pavleta Guokowa <pavleta.guokowa@gmail.com>
2022-08-08 10:09:26 +0300

    Test checkbox functionality

--- src/App.test.js
+++ src/App.test.js
@@ -13,3 +13,30 @@ test('button has correct initial color', () => {
   //expect the button text to be 'Change to red'
   expect(colorButton.textContent).toBe('Change to red');
 });
+
+test('initial conditions', () => {
+  render(<App />);
+  // check that the button starts enabled
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
+  // check that the checkbox starts unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+});
+ 
+test('Checkbox enables and disables a button', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  const checkbox = screen.getByRole('checkbox');
+  // click checkbox
+  fireEvent.click(checkbox);
+  // expect the checkbox to the checked
+  expect(checkbox).toBeChecked();
+  // expect the button to be disabled
+  expect(colorButton).toBeDisabled();
+  // click checkbox second time
+  fireEvent.click(checkbox);
+  // expect button to be enabled again
+  expect(colorButton).toBeEnabled();
+  expect(checkbox).not.toBeChecked();
+})
